2af8cb6f8877670d2af2cfa82bd2cc427ba2d7b6
Jesse Burdick-Pless <jb4411@rit.edu>
2020-06-12 19:54:33 -0400

    Worked on the Jukebox class

--- src/jukebox/Jukebox.java
+++ src/jukebox/Jukebox.java
@@ -10,12 +10,21 @@ public class Jukebox {
     ArrayList<Song> songList;
     HashSet<Song> songsPlayed;
     ArrayList<Song> firstFiveSongs;
+    Long simulations;
+    Long numSongsPlayed;
+    Song mostPlayed;
+    TreeSet<Song> songTree;
 
     public Jukebox(String filename, Integer seed) throws FileNotFoundException {
         Scanner in = new Scanner(new File(filename));
         this.random = new Random(seed);
         this.songList = new ArrayList<>();
         this.songs = new HashMap<>();
+        this.firstFiveSongs = new ArrayList<>();
+        this.simulations = 0L;
+        this.numSongsPlayed = 0L;
+        this.mostPlayed = null;
+        this.songTree = new TreeSet<>((a,b) -> a.song.compareTo(b.song));
 
         while (in.hasNext()) {
             String[] line = in.nextLine().split("<SEP>", 4);
@@ -25,37 +34,60 @@ public class Jukebox {
             }
         }
         this.songList.addAll(this.songs.keySet());
+        //this.songTree.addAll(this.songs.keySet());
     }
 
     public long play() {
         Long startTime = System.currentTimeMillis();
         for (int i = 0; i < 50000; i++) {
+            this.simulations += 1;
             this.songsPlayed = new HashSet<>();
             Song currentSong;
-            this.firstFiveSongs = new ArrayList<>();
             Boolean duplicate = false;
             while (!duplicate) {
                 currentSong = this.songList.get(this.random.nextInt(this.songList.size()));
-                if (this.firstFiveSongs.size() < 5) {
-                    this.firstFiveSongs.add(currentSong);
-                }
                 if (songsPlayed.contains(currentSong)) {
                     duplicate = true;
                 } else {
+                    if (this.firstFiveSongs.size() < 5) {
+                        this.firstFiveSongs.add(currentSong);
+                    }
                     this.songsPlayed.add(currentSong);
                     this.songs.replace(currentSong, this.songs.get(currentSong)+1);
+                    this.numSongsPlayed += 1;
+                    if (this.mostPlayed == null) {
+                        this.mostPlayed = currentSong;
+                    } else {
+                        if (this.songs.get(this.mostPlayed) < this.songs.get(currentSong)) {
+                            this.mostPlayed = currentSong;
+                        }
+                    }
                 }
             }
         }
         Long endTime = System.currentTimeMillis();
 
-        return endTime - startTime;
+        return (endTime - startTime)/1000;
     }
 
-    public void display(Long time) {
+    public void display(long time) {
         System.out.println("Printing first 5 songs played...");
         for (Song song : this.firstFiveSongs) {
-            System.out.println("    " + song.toString());
+            System.out.println("        " + song.toString());
+        }
+        System.out.println("Simulation took " + time +" second/s");
+        System.out.println("Number of simulations run: " + this.simulations);
+        System.out.println("Total number of songs played: " + this.numSongsPlayed);
+        System.out.println("Average number of songs played per simulation to get duplicate: " + (this.numSongsPlayed/this.simulations));
+        System.out.println("Most played song: \"" + this.mostPlayed.song + "\" by \"" + this.mostPlayed.artist + "\"");
+        System.out.println("All songs alphabetically by \"" + mostPlayed.artist + "\":");
+        for (Song song : this.songList) {
+            if (song.artist.equals(this.mostPlayed.artist)) {
+                this.songTree.add(song);
+            }
+        }
+        for (Song song : this.songTree) {
+            System.out.println("        \"" + song.song + "\" with " + this.songs.get(song) + " plays");
         }
 
     }
@@ -65,7 +97,7 @@ public class Jukebox {
         } else {
             Jukebox jukebox = new Jukebox(args[0], Integer.parseInt(args[1]));
             System.out.println("Jukebox of " + jukebox.songList.size() + " songs starts rockin'...");
-            Long time = jukebox.play();
+            long time = jukebox.play();
             jukebox.display(time);
         }
     }
