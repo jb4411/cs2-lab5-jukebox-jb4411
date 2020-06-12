06053ca21cf7929ccf3d6ffcc71c63018bc22be1
Jesse Burdick-Pless <jb4411@rit.edu>
2020-06-12 18:04:27 -0400

    Finished all methods in the Song class

--- src/jukebox/Song.java
+++ src/jukebox/Song.java
@@ -1,4 +1,49 @@
 package jukebox;
 
-public class Song {
+import java.util.Objects;
+
+public class Song implements Comparable<Song> {
+    String artist;
+    String song;
+
+    public Song(String artist, String song) {
+        this.artist = artist;
+        this.song = song;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getSong() {
+        return song;
+    }
+
+    @Override
+    public String toString() {
+        return "Artist: " + artist + ", Song: " + song;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Song song1 = (Song) object;
+        return artist.equals(song1.artist) &&
+                song.equals(song1.song);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.artist.hashCode() + this.song.hashCode();
+    }
+
+    @Override
+    public int compareTo(Song s) {
+        int result = this.artist.compareTo(s.artist);
+        if (result == 0) {
+            result = this.song.compareTo(s.song);
+        }
+        return result;
+    }
 }
